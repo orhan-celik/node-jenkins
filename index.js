926a12a7a63cba74f075af46f15f07cc805390bf
Orhan ÇELİK <44637777+orhan-celik@users.noreply.github.com>
2023-08-10 12:01:46 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Orhan ÇELİK');
+  res.send('Orhan ÇELİK :)');
 });
 
 app.listen(PORT, HOST, () => {
